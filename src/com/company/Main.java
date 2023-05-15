89650f558e4a1f3f049078d2ac230a63d822cff8
pocteg <pocteg@gmail.com>
2023-05-15 06:18:49 +0300

    Типы данных

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import com.company.lesson5.POJO;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Lesson #2 - POJO
+        POJO.run();
     }
 }
