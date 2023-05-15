81a567338d978f46c085f6654817ee2b83ff48ec
pocteg <pocteg@gmail.com>
2023-05-15 06:21:49 +0300

    Экземпляры класса

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,22 @@
 package com.company;
 
 import com.company.lesson5.POJO;
+import com.company.lesson6.Animal;
+import com.company.lesson6.Cat;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        // Lesson #2 - POJO
+        // Lesson #5 - POJO
         POJO.run();
+
+        // Lesson #6 - Экземпляры класса
+        Animal animal = new Animal();
+        animal.voice();
+
+        Cat cat = new Cat();
+        cat.voice();
     }
 }
